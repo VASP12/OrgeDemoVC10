c0406f557633762a1a132dd3ffe7bccbc8f9653c
yuqijun <force2008@gmail.com>
2013-04-12 17:57:07 +0800

    create ogre vc10

---
+++ OrgeDemoVC10/src/main.cpp
@@ -0,0 +1,27 @@
+#include "Ogre.h"
+#include "ExampleApplication.h"
+#include "BasicTutorial7.h"
+#include <windows.h>
+// First, we declare a subclass of the ExampleApplication class
+class FirstApp : public ExampleApplication
+{
+public:
+    FirstApp() { }
+
+    /** createScene is pure virtual in ExampleApplication, so we just override it to do nothing.
+    * What this means is that we'll be creating an empty scene at first.
+    **/
+    void createScene(void) { }
+};
+
+INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT){
+    try { // This try-catch provides a nice way of popping up errors if they occur.
+        //myApp.go(); // ExampleApplication provides a go method, which starts the rendering.
+		BasicTutorial7 app;  
+		app.go();
+    } catch (Ogre::Exception& e) {
+        MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!",
+        MB_OK | MB_ICONERROR | MB_TASKMODAL);
+    }
+	 return 0;
+}
